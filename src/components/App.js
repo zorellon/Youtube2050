8a2359384c41bdaa950c907de9e7296adb572fc3
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-02-14 14:30:35 -0500

    creating App component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div> This is the App compontent</div>
+        );
+    }
+}
+
+export default App;
